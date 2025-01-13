5f57be7c1ffb167cb7d21fa3c32fc934b04d8076
Kajitaka <142304219+kajitaka1201@users.noreply.github.com>
2025-01-13 07:59:14 +0000

    不要なフォント設定、インポートの削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui-elements/header";
 import Footer from "@/components/ui-elements/footer";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
